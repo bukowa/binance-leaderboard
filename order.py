c209d68dbc57ce1aab1ea238cc94ad7e7bd89a49
Buk Bukowski <gitbukowa@gmail.com>
2023-03-07 10:50:39 +0100

    add positions.json

--- order.py
+++ order.py
@@ -1,4 +1,9 @@
-import requests
+import dataclasses
+import time
+from dataclasses import dataclass
+
+import requests, json
+
 
 HEADERS = {
     "Cache-Control": "no-cache",
@@ -11,9 +16,10 @@ BODY = {
 
 API = "https://www.binance.com/bapi/futures/v1/public/future/leaderboard/getOtherPosition"
 
-USERS = {
-    "RealCryptrader": "765D3AE81B476C4EB14970053C6B8EC3",
-}
+
+with open('traders.json', 'r') as f:
+    traders = json.load(f)
+
 
 def get_body(uuid: str) -> dict:
     d = BODY.copy()
@@ -21,6 +27,34 @@ def get_body(uuid: str) -> dict:
     return d
 
 
-for k, v in USERS.items():
-    r = requests.post(API, json=get_body(v), headers=HEADERS)
-    print(f"{k}: {r.json()})")
+@dataclass
+class Position:
+    symbol: str
+    entryPrice: float
+    markPrice: float
+    pnl: float
+    roe: float
+    updateTime: list
+    amount: float
+    updateTimeStamp: int
+    yellow: bool
+    tradeBefore: bool
+    leverage: int
+    uuid: str
+
+positions = []
+
+for uuid, v in traders.items():
+    r = requests.post(API, json=get_body(uuid), headers=HEADERS)
+    print(f"{uuid}: {r.json()})")
+    data = r.json()['data'].get("otherPositionRetList")
+    if data:
+        for d in data:
+            d['uuid'] = uuid
+            positions.append(
+                dataclasses.asdict(Position(**d))
+            )
+
+with open('positions.json', 'w') as f:
+    json.dump(positions, f, indent='\t')
+
